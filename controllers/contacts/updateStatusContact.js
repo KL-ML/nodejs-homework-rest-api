2bf4b3dba3832de6efe42fd7c4a7c2f944be5621
Natalya Karpova <knataly0513@gmail.com>
2023-03-21 15:14:43 +0200

    Create updateStatusContact.js

---
+++ controllers/contacts/updateStatusContact.js
@@ -0,0 +1,18 @@
+const { catchAsync } = require('../../utils') 
+const Contact = require('../../models/contactModel')
+
+/**
+ * Update contact's status by id
+ */
+exports.updateStatusContact = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const { favorite } = req.body;
+
+  const updatedContactStatus = await Contact.findByIdAndUpdate(id, {
+    favorite
+  }, { new: true }).select('-__v');
+
+  res.status(200).json(
+    updatedContactStatus
+  );
+});
