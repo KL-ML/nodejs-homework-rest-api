c6e588135544519ac93056c44c4c6462496ba2b8
Natalya Karpova <knataly0513@gmail.com>
2023-03-21 15:31:55 +0200

    Create checkContactId.js

---
+++ middlewares/contacts/checkContactId.js
@@ -0,0 +1,23 @@
+const { Types } = require('mongoose');
+
+const Contact = require('../../models/contactModel');
+const { AppError, catchAsync } = require('../../utils');
+
+/**
+ * Check contact id.
+ */
+exports.checkContactId = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+
+  const idIsValid = Types.ObjectId.isValid(id);
+
+  // check if contact ID is invalid => send 'bad request' error
+  if (!idIsValid) return next(new AppError(404, 'Contact with this id does not exist..'));
+  
+  const contactExists = await Contact.exists({ _id: id });
+  // if contact exists => validation passed
+  if (contactExists) return next();
+
+  // if no contact with that id, sent 'not found' request
+  return next(new AppError(404, 'Contact with this id does not exist..'));
+});
