8ce5c2d583e702c27bbbdedb167ab222feae66c0
Natalya Karpova <knataly0513@gmail.com>
2023-03-21 15:14:30 +0200

    Create addContact.js

---
+++ controllers/contacts/addContact.js
@@ -0,0 +1,14 @@
+const { catchAsync } = require('../../utils') 
+const Contact = require('../../models/contactModel')
+
+/**
+ * Create contact.
+ */
+exports.addContact = catchAsync(async (req, res) => {
+    
+  const newContact = await Contact.create(req.body);
+
+  res.status(201).json(
+    newContact
+  );
+});
