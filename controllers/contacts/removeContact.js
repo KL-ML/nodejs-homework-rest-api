8f54cfac6d2f31d7cc4a58d81ecf4103bf36f9ff
Natalya Karpova <knataly0513@gmail.com>
2023-03-21 15:14:39 +0200

    Create removeContact.js

---
+++ controllers/contacts/removeContact.js
@@ -0,0 +1,15 @@
+const { catchAsync } = require('../../utils') 
+const Contact = require('../../models/contactModel')
+
+/**
+ * Delete contact by id.
+ */
+exports.removeContact = catchAsync(async (req, res) => {
+  const { id } = req.params;
+
+  await Contact.findByIdAndDelete(id);
+  
+  res.status(200).json({
+    "message": "contact deleted"
+  });
+});
