1d3e7b82f5445aa51844ffa3b7fcb6f1b0b706b7
Natalya Karpova <knataly0513@gmail.com>
2023-03-21 15:14:33 +0200

    Create getById.js

---
+++ controllers/contacts/getById.js
@@ -0,0 +1,15 @@
+const { catchAsync } = require('../../utils') 
+const Contact = require('../../models/contactModel')
+
+/**
+ * Get contact by id.
+ */
+exports.getById = catchAsync(async (req, res) => {
+  const { id } = req.params;
+
+  const contact = await Contact.findById(id).select('-__v');
+
+  res.status(200).json(
+    contact
+  );
+});
