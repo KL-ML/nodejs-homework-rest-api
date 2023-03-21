b597f06613d0cbb9ad97b92b55cfc803b27a51df
Natalya Karpova <knataly0513@gmail.com>
2023-03-21 15:31:58 +0200

    Create checkNewContactData.js

---
+++ middlewares/contacts/checkNewContactData.js
@@ -0,0 +1,15 @@
+const { AppError, catchAsync, validators } = require('../../utils');
+
+/**
+ * Check new contact data.
+ */
+exports.checkNewContactData = catchAsync(async (req, res, next) => {
+  // Check new contact data.
+  const { error, value } = validators.createContactValidator(req.body);
+
+  if (error) return next(new AppError(400, error.details[0].message));
+
+  req.body = value;
+
+  next();
+});
