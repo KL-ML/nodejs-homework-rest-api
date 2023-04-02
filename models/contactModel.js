c756594d85d14e0a280d14a86c474c451d14fa6e
Natalya Karpova <knataly0513@gmail.com>
2023-04-02 13:04:08 +0300

    Update contactModel.js

--- models/contactModel.js
+++ models/contactModel.js
@@ -16,10 +16,10 @@ const contactSchema = new mongoose.Schema({
         default: false,
     },
     owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    }
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
 },
 { versionKey: false, timestamps: true }
 );
